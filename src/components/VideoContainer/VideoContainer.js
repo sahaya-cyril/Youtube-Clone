bda637f3be002f3fcea3c4e85432fbbe76ae2ef9
Sahaya Cyril <sahayacyril2015@gmail.com>
2024-03-10 02:34:54 +0530

    YouTube: Created Iinfinite scroll using intersection observable

--- src/components/VideoContainer/VideoContainer.js
+++ src/components/VideoContainer/VideoContainer.js
@@ -1,18 +1,62 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import VideoCard from "../VideoCard/VideoCard";
 import { Link } from "react-router-dom";
 
 const VideoContainer = () => {
   const [videos, setVideos] = useState([]);
+  const [hasMore, setHasMore] = useState(false);
+  const [nextPage, setNextPage] = useState("");
+
+  const elementRef = useRef(null);
+
+  function onIntersection(entries) {
+    const firstEntry = entries[0];
+    if (firstEntry.isIntersecting && hasMore) {
+      fetchMoreVideos();
+    }
+  }
 
   useEffect(() => {
     getVideos();
   }, []);
 
+  useEffect(() => {
+    const observer = new IntersectionObserver(onIntersection);
+    if (observer && elementRef.current) {
+      observer.observe(elementRef.current);
+    }
+
+    return () => {
+      if (observer) {
+        observer.disconnect();
+      }
+    };
+  }, [videos]);
+
   const getVideos = async () => {
     const data = await fetch(process.env.REACT_APP_YOUTUBE_VIDEO_URL);
     const json = await data.json();
-    setVideos(json.items);
+    setVideos(json?.items);
+    if (json.nextPageToken) {
+      setNextPage(json?.nextPageToken);
+      setHasMore(true);
+    } else {
+      setHasMore(false);
+    }
+  };
+
+  const fetchMoreVideos = async () => {
+    const response = await fetch(
+      process.env.REACT_APP_YOUTUBE_MORE_VIDEO_URL + "&pageToken=" + nextPage
+    );
+    const json = await response.json();
+    if (json?.nextPageToken) {
+      setHasMore(true);
+      setNextPage(json?.nextPageToken);
+      setVideos((prevVideos) => [...prevVideos, ...json?.items]);
+    } else {
+      setHasMore(false);
+    }
   };
 
   return (
@@ -22,6 +66,7 @@ const VideoContainer = () => {
           <VideoCard key={video.id} info={video} />
         </Link>
       ))}
+      {hasMore && <div ref={elementRef}>Loading more items...</div>}
     </div>
   );
 };
